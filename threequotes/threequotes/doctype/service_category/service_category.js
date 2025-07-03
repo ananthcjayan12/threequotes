698a679946784bc2d0de8ae3ccd50556fa003248
ananthuuu <ananthcjayan12@gmail.com>
2025-07-03 10:20:11 +0000

    added first doctype

---
+++ threequotes/threequotes/doctype/service_category/service_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ananthuuuu and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Service Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ threequotes/threequotes/doctype/service_category/test_service_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, ananthuuuu and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestServiceCategory(FrappeTestCase):
+	pass
